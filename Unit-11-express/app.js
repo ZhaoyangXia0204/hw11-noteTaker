211531c6059b27f22c422a07d4146bc2fab95064
Zhaoyang Xia <zx70@scarletmail.rutgers.edu>
2020-02-25 09:50:37 -0500

    saving

---
+++ Unit-11-express/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+});
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+    return res.json(db);
+});
+
+app.post("/api/notes", (req, res) => {
+    let note = req.body;
+    db.push(note);
+
+    (async () => {
+        await fs.writeFile('db/db.json', JSON.stringify(db), err => {
+            if (err) throw err;
+        })
+    })()
+    res.json({ Ok: true });
+});
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+    console.log(__dirname);
+});
